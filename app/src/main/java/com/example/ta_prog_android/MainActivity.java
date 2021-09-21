456d80f5d72e5569f5c3d6b8daeb629fe275ca7d
AlexiaDOUILLARD <alexia.douillard@gmail.com>
2021-09-21 17:46:06 +0200

    MAJ page 1 + page3 + code java

--- app/src/main/java/com/example/ta_prog_android/MainActivity.java
+++ app/src/main/java/com/example/ta_prog_android/MainActivity.java
@@ -2,24 +2,36 @@ package com.example.ta_prog_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static String TAG = "UPConvTemp"; // Identifiant pour les messages de log
-    private EditText editInputTemp; // Boite de saisie de la température
-    private RadioButton rbCelsius; // Bouton radio indiquant si la saisie est en Celsius
-    private RadioButton rbFahrenheit; // Bouton radio indiquant si la saisie est en Fahrenheit
-    private TextView dispResult; // Le TextView qui affichera le résultat
+    public static String TAG = "Accueil"; // Identifiant pour les messages de log
+    private EditText Pageacceuil_Champsnom; // Boite de saisie de la température
+    private Button Pageacceuil_button_test; // Bouton radio indiquant si la saisie est en Celsius
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /**setContentView(R.layout.activity_main);**/
-        setContentView(R.layout.page1);
-        /**setContentView(R.layout.page2);**/
+        setContentView(R.layout.page3);
+        //Pageacceuil_button_test = findViewById(R.id.Pageacceuil_button_test);
+
+//        Pageacceuil_button_test.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                openNewActivity();
+//            }
+//        });
     }
+
+
+//    public void openNewActivity() {
+//        Intent intent = new Intent(this, First.class);
+//        startActivity(intent);
+//    }
 }
