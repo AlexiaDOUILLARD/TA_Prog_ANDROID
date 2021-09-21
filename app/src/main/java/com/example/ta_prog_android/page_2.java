1574a964ddab313aeb8949d8ade761e37aecc597
ASTIEeva <eva.astie@gmail.com>
2021-09-21 18:05:15 +0200

    page mon coeur + traduction

---
+++ app/src/main/java/com/example/ta_prog_android/page_2.java
@@ -0,0 +1,14 @@
+package com.example.ta_prog_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class page_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_2);
+    }
+}
