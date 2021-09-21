ac2532be06517e5d1e43b7a945c39366574dcb94
AlexiaDOUILLARD <alexia.douillard@gmail.com>
2021-09-21 09:49:18 +0200

    page profil OK (rotation + contraintes)

--- app/src/main/java/com/example/ta_prog_android/MainActivity.java
+++ app/src/main/java/com/example/ta_prog_android/MainActivity.java
@@ -3,8 +3,17 @@ package com.example.ta_prog_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static String TAG = "UPConvTemp"; // Identifiant pour les messages de log
+    private EditText editInputTemp; // Boite de saisie de la température
+    private RadioButton rbCelsius; // Bouton radio indiquant si la saisie est en Celsius
+    private RadioButton rbFahrenheit; // Bouton radio indiquant si la saisie est en Fahrenheit
+    private TextView dispResult; // Le TextView qui affichera le résultat
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
