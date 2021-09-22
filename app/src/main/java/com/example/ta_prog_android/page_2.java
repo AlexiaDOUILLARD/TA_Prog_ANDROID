841eb7cf22375e76357679ed9312edccd2598a52
ASTIEeva <eva.astie@gmail.com>
2021-09-22 15:59:33 +0200

    checkbox page 2

--- app/src/main/java/com/example/ta_prog_android/page_2.java
+++ app/src/main/java/com/example/ta_prog_android/page_2.java
@@ -2,13 +2,19 @@ package com.example.ta_prog_android;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class page_2 extends AppCompatActivity {
     private Button suivantpage2, precedentpage2;
+    private CheckBox checkBox1_page2;
+    private CheckBox checkBox2_page2;
+    private CheckBox checkBox3_page2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +23,12 @@ public class page_2 extends AppCompatActivity {
 
         suivantpage2 = findViewById(R.id.suivantpage2);
         precedentpage2 = findViewById(R.id.precedentpage2);
+        checkBox1_page2 = findViewById(R.id.checkBox1_page2);
+        checkBox2_page2 = findViewById(R.id.checkBox2_page2);
+        checkBox3_page2 = findViewById(R.id.checkBox3_page2);
+
+
+
 
         //Pour de passer de la page 2 à la page 3 **/
         suivantpage2.setOnClickListener(view -> {
@@ -30,7 +42,22 @@ public class page_2 extends AppCompatActivity {
             page_2.this.startActivity(intent);
         });
 
+    }
 
+    //Fonction pour cocher une seul check box à la fois
+    public void CheckBox_control (View v ){
+        if (checkBox1_page2.isSelected(true){
+            checkBox2_page2.setSelected(false);
+            checkBox3_page2.setSelected(false);
+
+        }else if (checkBox2_page2.isSelected()){
+            checkBox1_page2.setSelected(false);
+            checkBox3_page2.setSelected(false);
+        }else{
+            checkBox1_page2.setSelected(false);
+            checkBox2_page2.setSelected(false);
+
+        }
     }
 
 }
