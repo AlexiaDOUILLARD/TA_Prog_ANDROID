be54ba7fbd2e04670dd330bb3f96153a0d212615
AlexiaDOUILLARD <alexia.douillard@gmail.com>
2021-09-22 18:23:20 +0200

    résolution problème

--- app/src/main/java/com/example/ta_prog_android/page_2.java
+++ app/src/main/java/com/example/ta_prog_android/page_2.java
@@ -2,7 +2,6 @@ package com.example.ta_prog_android;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 
@@ -21,11 +20,11 @@ public class page_2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page2);
 
-//        suivantpage2 = findViewById(R.id.suivantpage2);
-//        precedentpage2 = findViewById(R.id.precedentpage2);
-//        checkBox1_page2 = findViewById(R.id.checkBox1_page2);
-//        checkBox2_page2 = findViewById(R.id.checkBox2_page2);
-//        checkBox3_page2 = findViewById(R.id.checkBox3_page2);
+        suivantpage2 = findViewById(R.id.suivantpage2);
+        precedentpage2 = findViewById(R.id.precedentpage2);
+        checkBox1_page2 = findViewById(R.id.checkBox1_page2);
+        checkBox2_page2 = findViewById(R.id.checkBox2_page2);
+        checkBox3_page2 = findViewById(R.id.checkBox3_page2);
 
 
 
@@ -43,20 +42,5 @@ public class page_2 extends AppCompatActivity {
         });
 
     }
-
-    //Fonction pour cocher une seul check box à la fois
-//    public void CheckBox_control (View v ){
-//        if (checkBox1_page2.isSelected(){
-//            checkBox2_page2.setSelected(false);
-//            checkBox3_page2.setSelected(false);
-//
-//        }else if (checkBox2_page2.isSelected()){
-//            checkBox1_page2.setSelected(false);
-//            checkBox3_page2.setSelected(false);
-//        }else{
-//            checkBox1_page2.setSelected(false);
-//            checkBox2_page2.setSelected(false);
-
         }
-    //}
 
