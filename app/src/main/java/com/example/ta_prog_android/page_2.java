4c6396279a9cbbff07ce4424b02364fe22b2060b
ASTIEeva <eva.astie@gmail.com>
2021-09-22 15:07:21 +0200

    correction code

--- app/src/main/java/com/example/ta_prog_android/page_2.java
+++ app/src/main/java/com/example/ta_prog_android/page_2.java
@@ -1,12 +1,12 @@
 package com.example.ta_prog_android;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+@SuppressWarnings("FieldCanBeLocal")
 public class page_2 extends AppCompatActivity {
     private Button suivantpage2;
     private Button precedentpage2;
@@ -18,22 +18,16 @@ public class page_2 extends AppCompatActivity {
         suivantpage2 = findViewById(R.id.suivantpage2);
         precedentpage2 = findViewById(R.id.precedentpage2);
 
-        /** Permet de passer de la page 2 à la page 3 **/
-        suivantpage2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(page_2.this, page_3.class);
-                page_2.this.startActivity(intent);
-            }
+        //Pour de passer de la page 2 à la page 3 **/
+        suivantpage2.setOnClickListener(view -> {
+            Intent intent = new Intent(page_2.this, page_3.class);
+            page_2.this.startActivity(intent);
         });
 
-        /** Permet de passer de la page 2 à la page 1**/
-        precedentpage2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(page_2.this, page_1.class);
-                page_2.this.startActivity(intent);
-            }
+        // Permet de passer de la page 2 à la page 1**/
+        precedentpage2.setOnClickListener(view -> {
+            Intent intent = new Intent(page_2.this, page_1.class);
+            page_2.this.startActivity(intent);
         });
 
 
