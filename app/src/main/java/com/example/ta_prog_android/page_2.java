bbfad08163ed0751aa929d154c31a39ec92be50e
ASTIEeva <eva.astie@gmail.com>
2021-09-22 14:48:45 +0200

    BOUTON

--- app/src/main/java/com/example/ta_prog_android/page_2.java
+++ app/src/main/java/com/example/ta_prog_android/page_2.java
@@ -6,27 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.Spinner;
 
 public class page_2 extends AppCompatActivity {
-    private Spinner spinner3, spinner2, spinner4, spinner8, spinner6;
-    private CheckBox checkBox1,checkBox2,checkBox3;
-    private Button precedentpage2, suivantpage2;
+    private Button suivantpage2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page2);
+        suivantpage2 = findViewById(R.id.suivantpage2);
 
+        /** Permete de passer de la page 2 à la page 3**/
         suivantpage2.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                openNewActivity();
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(page_2.this, page_3.class);
+                page_2.this.startActivity(intent);
             }
         });
+
     }
-    public void openNewActivity() {
-        Intent intent = new Intent(this, page_3.class);
-        startActivity(intent);
-    }
+
 }
