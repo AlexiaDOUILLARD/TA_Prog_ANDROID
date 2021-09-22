174c741a6646000cc39fd95546dccce8061af8a6
ASTIEeva <eva.astie@gmail.com>
2021-09-22 14:54:06 +0200

    BOUTON

--- app/src/main/java/com/example/ta_prog_android/page_2.java
+++ app/src/main/java/com/example/ta_prog_android/page_2.java
@@ -9,14 +9,16 @@ import android.widget.Button;
 
 public class page_2 extends AppCompatActivity {
     private Button suivantpage2;
+    private Button precedentpage2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page2);
         suivantpage2 = findViewById(R.id.suivantpage2);
+        precedentpage2 = findViewById(R.id.precedentpage2);
 
-        /** Permete de passer de la page 2 à la page 3**/
+        /** Permet de passer de la page 2 à la page 3**/
         suivantpage2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -24,6 +26,16 @@ public class page_2 extends AppCompatActivity {
                 page_2.this.startActivity(intent);
             }
         });
+
+        /** Permet de passer de la page 2 à la page 1**/
+        precedentpage2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(page_2.this, page_1.class);
+                page_2.this.startActivity(intent);
+            }
+        });
+
 
     }
 
