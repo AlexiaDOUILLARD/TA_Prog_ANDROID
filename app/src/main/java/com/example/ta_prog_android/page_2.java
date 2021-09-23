634d24247ad56f3580a7fc9c302c5217401c9300
ASTIEeva <eva.astie@gmail.com>
2021-09-23 21:24:24 +0200

    contrainte checkbox

--- app/src/main/java/com/example/ta_prog_android/page_2.java
+++ app/src/main/java/com/example/ta_prog_android/page_2.java
@@ -2,6 +2,7 @@ package com.example.ta_prog_android;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 
@@ -13,6 +14,9 @@ public class page_2 extends AppCompatActivity {
     private CheckBox checkBox1_page2;
     private CheckBox checkBox2_page2;
     private CheckBox checkBox3_page2;
+    private Boolean BooleanCheck = false;
+    private Boolean BooleanCheck2 = false;
+    private Boolean BooleanCheck3 = false;
 
 
     @Override
@@ -41,6 +45,46 @@ public class page_2 extends AppCompatActivity {
             page_2.this.startActivity(intent);
         });
 
+        // Listener sur les 3 CheckBox
+        checkBox1_page2.setOnClickListener(v -> {
+            //change boolean value
+            BooleanCheck=true;
+        });
+
+        checkBox2_page2.setOnClickListener(v -> {
+            //change boolean value
+            BooleanCheck2=true;
+        });
+
+        checkBox3_page2.setOnClickListener(v -> {
+            //change boolean value
+            BooleanCheck3=true;
+        });
+
+
+
     }
+
+    //Fonction pour cocher une seul check box à la fois
+    public Boolean CheckBox_control () {
+        if (BooleanCheck = true) {
+            checkBox2_page2.setSelected(false);
+            checkBox3_page2.setSelected(false);
+            return false;
+
         }
+        if (BooleanCheck2 = true) {
+            checkBox1_page2.setSelected(false);
+            checkBox3_page2.setSelected(false);
+            return false;
+        }
+        if (BooleanCheck3 = true) {
+            checkBox1_page2.setSelected(false);
+            checkBox2_page2.setSelected(false);
+            return false;
+
+        }
+        return true;
+    }
+}
 
