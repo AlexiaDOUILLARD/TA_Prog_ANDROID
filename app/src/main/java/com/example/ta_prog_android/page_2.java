99349c8569be4cb01feb559281ec8bc2aad15c79
AlexiaDOUILLARD <alexia.douillard@gmail.com>
2021-09-24 16:19:05 +0200

    avancement projet

--- app/src/main/java/com/example/ta_prog_android/page_2.java
+++ app/src/main/java/com/example/ta_prog_android/page_2.java
@@ -2,23 +2,20 @@ package com.example.ta_prog_android;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class page_2 extends AppCompatActivity {
-
-    private static final String TAG = "Page_2 Stat :";
     private Button suivantpage2, precedentpage2;
-    private CheckBox checkBox1_page2, checkBox2_page2, checkBox3_page2;
-    boolean BooleanCheck = false;
-    boolean BooleanCheck2 = false;
-    boolean BooleanCheck3 = false;
-    boolean checkBoxControl = false;
+    private CheckBox checkBox1_page2;
+    private CheckBox checkBox2_page2;
+    private CheckBox checkBox3_page2;
+    private Boolean BooleanCheck = false;
+    private Boolean BooleanCheck2 = false;
+    private Boolean BooleanCheck3 = false;
 
 
     @Override
@@ -32,15 +29,13 @@ public class page_2 extends AppCompatActivity {
         checkBox2_page2 = findViewById(R.id.checkBox2_page2);
         checkBox3_page2 = findViewById(R.id.checkBox3_page2);
 
-        processIntentData();
+
+
 
         //Pour de passer de la page 2 à la page 3 **/
         suivantpage2.setOnClickListener(view -> {
-            checkBoxControl = CheckBox_control();
-            if (checkBoxControl) {
-                Intent intent = new Intent(page_2.this, page_3.class);
-                page_2.this.startActivity(intent);
-            }
+            Intent intent = new Intent(page_2.this, page_3.class);
+            page_2.this.startActivity(intent);
         });
 
         // Permet de passer de la page 2 à la page 1**/
@@ -50,75 +45,51 @@ public class page_2 extends AppCompatActivity {
         });
 
         // Listener sur les 3 CheckBox
-        checkBox1_page2.setOnClickListener(view -> BooleanCheck = ((CheckBox) view).isChecked());
+        checkBox1_page2.setOnClickListener(v -> {
+            //change boolean value
+            BooleanCheck=true;
+        });
 
-        checkBox2_page2.setOnClickListener(view -> BooleanCheck2 = ((CheckBox) view).isChecked());
+        checkBox2_page2.setOnClickListener(v -> {
+            //change boolean value
+            BooleanCheck2=true;
+        });
 
-        checkBox3_page2.setOnClickListener(view -> BooleanCheck3 = ((CheckBox) view).isChecked());
+        checkBox3_page2.setOnClickListener(v -> {
+            //change boolean value
+            BooleanCheck3=true;
+        });
+
+
 
     }
 
     //Fonction pour cocher une seul check box à la fois
-    public boolean CheckBox_control() {
-        if ((!BooleanCheck) & (!BooleanCheck2) & (!BooleanCheck3)) {
-            Toast.makeText(page_2.this, "Please answer the last question by checking a box", Toast.LENGTH_LONG).show();
+    public Boolean CheckBox_control () {
+        if (BooleanCheck = true) {
+            checkBox2_page2.setSelected(false);
+            BooleanCheck2 = false;
+            checkBox3_page2.setSelected(false);
+            BooleanCheck3 = false;
+            return false;
+
+        }
+        if (BooleanCheck2 = true) {
+            checkBox1_page2.setSelected(false);
+            BooleanCheck = false;
+            checkBox3_page2.setSelected(false);
+            BooleanCheck3 =false;
             return false;
         }
-        if (BooleanCheck & BooleanCheck2 & !BooleanCheck3) {
-            Toast.makeText(page_2.this, "You cannot check two boxes for the last question", Toast.LENGTH_LONG).show();
+        if (BooleanCheck3 = true) {
+            checkBox1_page2.setSelected(false);
+            BooleanCheck = false;
+            checkBox2_page2.setSelected(false);
+            BooleanCheck2 = false;
             return false;
-        }
-        if (BooleanCheck & BooleanCheck3 & !BooleanCheck2) {
-            Toast.makeText(page_2.this, "You cannot check two boxes for the last question", Toast.LENGTH_LONG).show();
-            return false;
-        }
-        if (BooleanCheck & BooleanCheck2 & BooleanCheck3) {
-            Toast.makeText(page_2.this, "You cannot check two boxes for the last question", Toast.LENGTH_LONG).show();
-            return false;
+
         }
         return true;
-    }
-
-    // This method (whose name is abritrary) is called by onCreate().
-    private void processIntentData() {
-        Intent intent = getIntent();
-        if (intent != null) {
-            // intent may store different data. To get the one matching the Person class,
-            // we need the key "FromActivity1ToActivity2" which was used for transfer
-            // No need to calls "new()" for allocating memory to transferredPerson
-            Person transferredPerson = intent.getParcelableExtra("FromAccueilToPage1");
-            if (transferredPerson != null) {
-                transferredPerson.print();
-            } else {
-                Log.d(page_2.TAG, "No Person found after transfer from Activity1");
-            }
-        } else {
-            Log.d(page_2.TAG, "Error when transferring from Activity1");
-        }
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG,"onPause :" );
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(TAG,"onStart :" );
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG,"onDestroy :" );
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG,"onResume :" );
     }
 }
 
